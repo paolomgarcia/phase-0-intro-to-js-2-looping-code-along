66f49871e6ca42efe68d3e510eb443664d95c7e3
Paolo Garcia <pmatteo1108@yahoo.com>
2022-09-06 16:10:30 -0600

    commit tree 23ca00fd5ed59c15e100fbefd96d99eed1b91304

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  };
+  return messages;
+}
+console.log(messages);
+
+
+function countDown(dec) {
+    let i = 10
+    while (i >= 0) {
+        console.log(i--);
+    }
+        return dec;
+    }
+    console.log(countDown(10));
+    
+    
+    
+
